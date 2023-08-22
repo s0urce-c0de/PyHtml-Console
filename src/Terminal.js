251fe91b3a1cb40505da324fccfe863deeb6db82
s0urce-c0de <email.kanavg@gmail.com>
2023-08-21 17:28:39 -0700

    ️

--- src/Terminal.js
+++ src/Terminal.js
@@ -1,16 +1,20 @@
 import React, { useEffect, useRef } from 'react';
 import { Terminal as XTerm } from 'xterm';
+import { FitAddon } from 'xterm-addon-fit'
 import 'xterm/css/xterm.css';
 import './Terminal.css'
 import { loadPyodide } from 'pyodide';
 
+
 const term = new XTerm();
+term.fitAddon = new FitAddon()
+term.loadAddon(term.fitAddon)
+
 term.prompt = () => {
   term.write('\n\r>>> ')
 }
-const Terminal = () => {
+export default function Terminal(){
   const termRef = useRef(null);
-  var curr_line = '';
   useEffect(() => {
 
     // Attach the term to the DOM
@@ -20,38 +24,43 @@ const Terminal = () => {
 
     // Load Pyodide
     const pyodide = (async function() {
-      const pyodide = await loadPyodide({ indexURL: 'https://cdn.jsdelivr.net/pyodide/v0.23.4/full/' }).then(function(p){window.pyodide=p;return p});
+      const pyodide = await loadPyodide({
+        indexURL: 'https://cdn.jsdelivr.net/pyodide/v0.23.4/full/',
+        stdout: (msg) => {
+          term.write(msg)
+          term.prompt()
+        }
+      }).then(function(p){window.pyodide=p;return p});
       var pyodideReady = true;
       return pyodide
     })()
-
+    term.curr_line='';
     // Send term input to Pyodide for execution
     term.onKey(function(ev) {
-      if (ev.key === '\r') {
-        execpy(curr_line)
-        curr_line=''
+      if (ev.key === '\r' || ev.key === '\n') {
+        execpy(term.curr_line)
+        term.curr_line=''
         term.prompt()
-      } else if (ev.key === '\x7f' || ev.key === '\x08') {
-        term.write('\b \b')
-        curr_line=curr_line.slice(0, -1)
+      } else if (ev.key === '\x7f') {
+        if (term.curr_line !== '') {term.write('\b \b')}
+        term.curr_line=term.curr_line.slice(0,-1)
       } else {
-        curr_line += ev.key
-        term.write((ev.key))
+        term.curr_line += ev.key
+        term.write(ev.key)
       }
-      console.log(ev);
     })
     // Execute Python code using Pyodide
     const execpy = (code) => {
-      console.log(window.pyodide === pyodide)
       window.pyodide.runPythonAsync(code).then((result) => {
-        term.writeln(result);
+        console.log(result)
+        term.write(result);
       }).catch(error => {
-        term.writeln(`Error: ${error}`);
-      }).finally(() => {
-        term.write(`\n${term.prompt}`);
-      });
+        console.log(error)
+        term.write(`\n${error.message}`);
+      })
     };
 
+    term.fitAddon.fit()
     term.writeln('Python React Terminal');
     term.prompt();
 
@@ -63,5 +72,3 @@ const Terminal = () => {
 
   return <div id="terminal" ref={termRef} />;
 };
-
-export default Terminal;
